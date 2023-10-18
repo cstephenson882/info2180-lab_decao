7c83572a19ec2f0444731dee33c239a791da88b5
calvin_udemy_python <cstephenson882@gmail.com>
2023-10-18 15:18:56 -0500

    Add an X or O to a square when clicked

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -11,15 +11,56 @@
 
 // on load this event run to load the sqaures
 
-document.addEventListener('DOMContentLoaded', function () {
-    const boardParent = document.querySelector('#board');
-    var boardElement = document.getElementById('board');
-    var boardChildren = boardElement.children;
-    Array.from(boardChildren).forEach(function(child) {
-        // Your code to process each child goes here
-        child.classList.add('square');
-    });
+function checkWin() {
+    //alert(9999);
 
- }); 
+}
+document.addEventListener('DOMContentLoaded', function () {
+    
+
+    var switch_ = 0;
+    var state_ = [-1,-1,-1,-1,-1,-1,-1,-1,-1];
+    console.log(Array.from(state_));
+
+  
+   
+    // Select the "board" element using querySelector
+    var boardElement = document.querySelector('#board');
+    // Get the children of the "board" element
+    var boardChildren = boardElement.children;
+
+    // Iterate over each child and add the "square" class
+    Array.from(boardChildren).forEach(function(child) {
+        child.classList.add('square');
+
+        child.addEventListener('click', function() {
+          var index = Array.from(boardChildren).indexOf(child);
+          console.log(`Square ${index} clicked!`);
+            // Your code to handle the click event goes here
+
+            // Add the "O" class only to the clicked square
+            if (switch_ == 0) {
+                child.classList.add('O');
+                let myText = document.createTextNode("O");
+                child.append(myText);
+                switch_= 1;
+                state_[index] = 0;
+                //child.style.backgroundColor = 'yellow';
+            } else{
+                child.classList.add('X');
+                let myText = document.createTextNode("X");
+                child.append(myText);
+                switch_ = 0;
+                state_[index] = 1;
+            }
+            console.log(Array.from(state_));
+          
+            checkWin();
+        });
+    });
+});
+
+
+ 
 
  
